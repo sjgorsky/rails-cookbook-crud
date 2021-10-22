a157402ce48af9ef9ec7062bcebb85d4b6d4e5b6
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2021-10-22 10:58:23 -0500

    adding backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :cookbook_collections
-  resources :cookbooks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cookbook_collections#index'
 end
