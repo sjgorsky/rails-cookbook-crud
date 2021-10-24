aa77cff283d8f048433d88b9f526e3f70a1a6518
Scott Gorsky <scottgorsky@Scott-Gorskys-MacBook-Pro.local>
2021-10-24 14:37:03 -0500

    routes

--- app/controllers/api/v1/cookbook_collections_controller.rb
+++ app/controllers/api/v1/cookbook_collections_controller.rb
@@ -18,5 +18,48 @@ class Api::V1::CookbookCollectionsController < ApplicationController
         end
     end
 
+    #GET /cookbook_collections/new
+    def new
+        @cookbook_collection = CookbookCollection.new
+    end
+
+    #GET /cookbook_collections/1/edit
+    def edit
+    end
+
+    #POST /cookbook_collections
+    #POST /cookbook_collections.json
+    def create
+        @cookbook_collection = CookbookCollection.new(cookbook_params)
+
+        if @cookbook_collection.save
+            render json: @cookbook_collection
+        else
+            render json: @cookbook_collection.errors
+        end
+    end
+
+    #PATCH/PUT /cookbook_collections/1
+    #PATCH/PUT /cookbook_collections/1.json
+    def update
+    end
+
+    #DELETE /cookbook_collections/1
+    #DELETE /cookbook_collections/1.json
+    def destroy
+        @cookbook_collection.destroy
+
+        render json: { notice: 'Cookbook was successfully removed' }
+    end
+
+    private
+    def set_cookbook
+        @cookbook_collection = CookbookCollection.find(params[:id])
+    end
+
+    #only allow a list of trusted parameters through
+    def cookbook_params
+        params.permit(:title, :author, :cuisine, :year)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root 'cookbook_collections#index'
 end
+
+#   namespace :api do
+#     namespace :v1 do
+#       get 'cookbook_collections/index'
+#       post 'cookbook_collections/create'
+#       delete 'cookbook_collections/:id', to: 'cookbook_collections#destroy'
+#     end
+#   end
+
+#   root 'cookbook_collections#index'
+# end
